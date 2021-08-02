43bfa08818fc19de6d9f6a2e4e99896bbc770128
Igor <igor.marinho@fortbrasil.com.br>
2021-08-02 19:17:09 -0300

    style: global styles and theme colors

---
+++ src/pages/_document.tsx
@@ -0,0 +1,28 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* <link rel="manifest" href="/manifest.json" /> */}
+          {/* <link rel="apple-touch-icon" href="/icon.png"></link> */}
+          {/* <link rel="shortcut icon" href="/favicon.png" type="image/png" /> */}
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Quicksand&family=Rubik:wght@500&display=swap"
+            rel="stylesheet"
+          />
+          <meta name="theme-color" content="#fff" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
 import { ReactNode } from 'react'
-import styles from '../styles/Home.module.scss'
+import styles from '../styles/home.module.scss'
 
 export default function Home(): ReactNode {
   return (
-    <>
+    <div className={styles.container}>
       <h1>Hello World</h1>
-    </>
+    </div>
   )
 }
