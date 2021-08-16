f6ab519eba8f15a6e110c6caac06c77c07aed838
Igor <igor.marinho@fortbrasil.com.br>
2021-08-16 20:39:36 -0300

    feat: player context provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,18 +3,22 @@ import type { AppProps } from 'next/app'
 import Header from '../components/Header'
 import Player from '../components/Player'
 
+import { PlayerContextProvider } from '../context/PlayerContext'
+
 import styles from '../styles/app.module.scss'
 import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
-    <div className={styles.container}>
-      <Header />
-      <main>
-        <Component {...pageProps} />
-      </main>
-      <Player />
-    </div>
+    <PlayerContextProvider>
+      <div className={styles.container}>
+        <Header />
+        <main>
+          <Component {...pageProps} />
+        </main>
+        <Player />
+      </div>
+    </PlayerContextProvider>
   )
 }
 export default MyApp
