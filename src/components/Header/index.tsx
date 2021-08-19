ab7d1a7e52d6db0e0987349a65a5b48d0fe8061e
Igor <igor.marinho@fortbrasil.com.br>
2021-08-19 19:51:08 -0300

    feat: redirect to home

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import Image from 'next/image'
 import format from 'date-fns/format'
 import ptBR from 'date-fns/locale/pt-BR'
@@ -11,18 +12,21 @@ const Header = (): JSX.Element => {
 
   return (
     <header className={styles.container}>
-      <div className={styles.logo}>
-        <Image
-          src="/logo-65x65.svg"
-          alt="logo"
-          width={65}
-          height={65}
-          quality={100}
-        />
-        <h1>
-          Olá, ambiente <br></br> Sou eu de novo.
-        </h1>
-      </div>
+      <Link href="/" passHref>
+        <div className={styles.logo}>
+          <Image
+            src="/logo-65x65.svg"
+            alt="logo"
+            width={65}
+            height={65}
+            quality={100}
+          />
+          <h1>
+            Olá, ambiente <br></br> Sou eu de novo.
+          </h1>
+        </div>
+      </Link>
+
       <span>{currentDate}</span>
     </header>
   )
