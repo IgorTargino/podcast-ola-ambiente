52e0ba8f7720e378c78f4667d2838ed50dda8368
IgorTargino <igortargino01@gmail.com>
2021-08-20 16:32:03 -0300

    styles: remove hours to time

--- src/utils/convertDurationToTimeString.ts
+++ src/utils/convertDurationToTimeString.ts
@@ -1,9 +1,8 @@
 const convertDurationToTimeString = (duration: number): string => {
-  const hours = Math.floor(duration / 3600)
   const minutes = Math.floor((duration % 3600) / 60)
   const seconds = duration % 60
 
-  const timeString = [hours, minutes, seconds]
+  const timeString = [minutes, seconds]
     .map((unit) => String(unit).padStart(2, '0'))
     .join(':')
 
